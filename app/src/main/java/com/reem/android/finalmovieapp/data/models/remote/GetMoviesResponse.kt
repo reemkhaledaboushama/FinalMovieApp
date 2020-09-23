2864a634ad0f7e0cbffbcfa490542347e44b48e9
reemk <reemkhaledaboushama@gmail.com>
2020-09-23 23:08:32 +0200

    - added data class GetMoviesResponse.kt

---
+++ app/src/main/java/com/reem/android/finalmovieapp/data/models/remote/GetMoviesResponse.kt
@@ -0,0 +1,14 @@
+package com.reem.android.finalmovieapp.data.models.remote
+
+import com.google.gson.annotations.SerializedName
+import com.reem.android.finalmovieapp.data.models.ui.Movie
+
+data class GetMoviesResponse(
+    @SerializedName("page")
+    val page: Int,
+    @SerializedName("results")
+
+    val movies: List<Movie>,
+    @SerializedName("total_pages")
+    val pages: Int
+)

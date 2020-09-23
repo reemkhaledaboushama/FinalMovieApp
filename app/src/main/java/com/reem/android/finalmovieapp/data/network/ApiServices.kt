a2fde5124110cb9163bb14aff637f7311e8ca36a
reemk <reemkhaledaboushama@gmail.com>
2020-09-23 23:07:41 +0200

    - networking implementation (ApiServices and RetrofitClient.kt classes )

--- app/src/main/java/com/reem/android/finalmovieapp/data/network/ApiServices.kt
+++ app/src/main/java/com/reem/android/finalmovieapp/data/network/ApiServices.kt
@@ -1,4 +1,24 @@
 package com.reem.android.finalmovieapp.data.network
+import com.reem.android.finalmovieapp.data.models.remote.GetMoviesResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiServices {
+    @GET("movie/popular")
+    fun getPopularMovies(
+        @Query("api_key") apiKey: String = "d78d306988042530d99e166c3cb00017",
+        @Query("page") page: Int
+    ): Call<GetMoviesResponse>
+
+    @GET("movie/top_rated")
+    fun getTopRatedMovies(
+        @Query("api_key") apiKey: String = "d78d306988042530d99e166c3cb00017",
+        @Query("page") page: Int
+    ): Call<GetMoviesResponse>
+
+
+
+
+
 }

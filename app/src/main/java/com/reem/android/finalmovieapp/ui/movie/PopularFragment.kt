ce1f927df9d62ef160eb7a6aa7af808c49059351
steeevvv <stevesame2040@gmail.com>
2020-09-24 18:39:26 +0200

    Ended Popular Fragment + Movies Adapter

--- app/src/main/java/com/reem/android/finalmovieapp/ui/movie/PopularFragment.kt
+++ app/src/main/java/com/reem/android/finalmovieapp/ui/movie/PopularFragment.kt
@@ -1,13 +1,19 @@
 package com.reem.android.finalmovieapp.ui.movie
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.reem.android.finalmovieapp.R
+import com.reem.android.finalmovieapp.data.models.ui.Movie
+import com.squareup.picasso.Picasso
 
 class PopularFragment : Fragment() {
 
@@ -34,6 +40,52 @@ class PopularFragment : Fragment() {
         popularMoviesRecyclerView = view.findViewById(R.id.movies_recycler_view)
 
         popularMoviesRecyclerView.layoutManager=LinearLayoutManager(context)
+    }
+
+    class MoviesAdapter (private val movies: List<Movie>) :
+            RecyclerView.Adapter<MoviesAdapter.PopularMoviesViewHolder>() {
+
+            inner class PopularMoviesViewHolder (itemView: View): RecyclerView.ViewHolder(itemView){
+
+                private val movieTitleTextView: TextView =
+                    itemView.findViewById(R.id.movie_title_text_view)
+                private val movieRatingTextView: TextView = itemView.findViewById(R.id.movie_rating)
+                private val movieReleaseDateTextView: TextView =
+                    itemView.findViewById(R.id.movie_release_date)
+                private val movieLanguageTextView: TextView = itemView.findViewById(R.id.movie_language)
+                private val movieImageView: ImageView = itemView.findViewById(R.id.movie_image_view)
+
+                @SuppressLint("SetTextI18n")
+                fun bind(movie: Movie) {
+                    movieTitleTextView.text = movie.title
+                    movieRatingTextView.text = "${movie.vote_average}/10"
+                    movieReleaseDateTextView.text = movie.release_date
+                    movieLanguageTextView.text = movie.original_language
+
+                    val url = "https://image.tmdb.org/t/p/w500".plus(movie.poster_path)
+
+                    Picasso.get()
+                        .load(url)
+                        .placeholder(R.drawable.ic_loading)
+                        .into(movieImageView)
+                }
+
+            }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PopularMoviesViewHolder {
+            val cardView = LayoutInflater.from(parent.context)
+                .inflate(R.layout.movie_item, parent, false) as CardView
+            return PopularMoviesViewHolder(cardView)
+        }
+
+        override fun onBindViewHolder(holder: PopularMoviesViewHolder, position: Int) {
+            val movie = movies[holder.adapterPosition]
+            holder.bind(movie)
+        }
+
+        override fun getItemCount(): Int {
+            return movies.size
+        }
     }
 
 

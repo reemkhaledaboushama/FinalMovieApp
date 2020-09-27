d07981fbab840186e2428e671b5a141cfe2ea4fd
reemk <reemkhaledaboushama@gmail.com>
2020-09-28 01:26:07 +0200

    - refactored code in PopularFragment.kt, took relevant code from MainActivity.kt and into PopularFragment.kt

--- app/src/main/java/com/reem/android/finalmovieapp/ui/movie/PopularFragment.kt
+++ app/src/main/java/com/reem/android/finalmovieapp/ui/movie/PopularFragment.kt
@@ -1,22 +1,35 @@
 package com.reem.android.finalmovieapp.ui.movie
 
+
+import android.content.res.Configuration
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.LinearLayout
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.reem.android.finalmovieapp.R
+import com.reem.android.finalmovieapp.data.models.remote.*
+import com.reem.android.finalmovieapp.data.models.ui.Movie
+import com.reem.android.finalmovieapp.data.repository.MoviesRepository
 import com.reem.android.finalmovieapp.ui.MoviesAdapter
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.popular_fragment.*
 
 
 class PopularFragment: Fragment() {
 
-    private lateinit var popularMoviesRecyclerView: RecyclerView
+    private lateinit var popularMovies: RecyclerView
     private lateinit var popularMoviesAdapter: MoviesAdapter
-
+    private lateinit var popularMoviesLayoutMgr: LinearLayoutManager
+    private lateinit var popularMovieGrid: GridLayoutManager
+    private var popularMoviesPage = 1
 
 
     override fun onCreateView(
@@ -24,19 +37,73 @@ class PopularFragment: Fragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
 
+
         return inflater.inflate(R.layout.popular_fragment, container, false)
-
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
 
-
-
-       popularMoviesRecyclerView = view.findViewById(R.id.popular_movies)
-        popularMoviesRecyclerView.layoutManager = LinearLayoutManager(context)
+    private fun showMovieDetails(movie: Movie) {
+        val intent = Intent(this.context, MovieDetailsActivity::class.java)
+        intent.putExtra(MOVIE_BACKDROP, movie.backdropPath)
+        intent.putExtra(MOVIE_POSTER, movie.posterPath)
+        intent.putExtra(MOVIE_TITLE, movie.title)
+        intent.putExtra(MOVIE_RATING, movie.rating)
+        intent.putExtra(MOVIE_RELEASE_DATE, movie.releaseDate)
+        intent.putExtra(MOVIE_OVERVIEW, movie.overview)
+        startActivity(intent)
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        popularMovies = recycle_popular_movies
+      popularMovieGrid = GridLayoutManager(this.context, 2)
+        popularMoviesLayoutMgr = LinearLayoutManager(
+            this.context,
+            LinearLayoutManager.VERTICAL,
+            false
+        )
+        popularMovies.layoutManager = popularMoviesLayoutMgr
+        popularMoviesAdapter = MoviesAdapter(mutableListOf()) { movie -> showMovieDetails(movie) }
+        println(popularMoviesAdapter)
+        popularMovies.adapter = popularMoviesAdapter
+        getPopularMovies()
+    }
+
+    private fun getPopularMovies() {
+        MoviesRepository.getPopularMovies(
+            popularMoviesPage,
+            ::onPopularMoviesFetched,
+            ::onError
+        )
+    }
+
+    private fun onPopularMoviesFetched(movies: List<Movie>) {
+        popularMoviesAdapter.appendMovies(movies)
+        attachPopularMoviesOnScrollListener()
+    }
+
+    private fun onError() {
+        Toast.makeText(this.context, getString(R.string.error_fetch_movies), Toast.LENGTH_SHORT).show()
+    }
+
+    private fun attachPopularMoviesOnScrollListener() {
+        popularMovies.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                val totalItemCount = popularMoviesLayoutMgr.itemCount
+                val visibleItemCount = popularMoviesLayoutMgr.childCount
+                val firstVisibleItem = popularMoviesLayoutMgr.findFirstVisibleItemPosition()
+
+                if (firstVisibleItem + visibleItemCount >= totalItemCount / 2) {
+                    popularMovies.removeOnScrollListener(this)
+                    popularMoviesPage++
+                    getPopularMovies()
+                }
+            }
+        })
+    }
+
 }
+
 
 
 

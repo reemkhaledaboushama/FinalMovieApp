fd3cd9e80324155152cdb07d0a8e926653ad49b3
olaalnajjar <69318042+olaalnajjar@users.noreply.github.com>
2020-09-29 14:15:32 +0200

    Adding Dao Functions for new Top Rated DataBase

--- app/src/main/java/com/reem/android/finalmovieapp/data/database/MovieDao.kt
+++ app/src/main/java/com/reem/android/finalmovieapp/data/database/MovieDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.reem.android.finalmovieapp.data.models.ui.Movie
+import com.reem.android.finalmovieapp.data.models.ui.MovieT
 
 @Dao
 interface MovieDao {
@@ -14,10 +15,14 @@ interface MovieDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(movies: List<Movie>)
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAllT(movies: List<MovieT>)
+
     @Query("SELECT * FROM movies_table")
     fun getAllMovies(): List<Movie>
 
-
+    @Query("SELECT * FROM moviesT_table")
+    fun getAllMoviesT(): List<MovieT>
 
 
 }

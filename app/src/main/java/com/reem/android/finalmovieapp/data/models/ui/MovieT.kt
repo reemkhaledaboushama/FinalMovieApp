3796124c94753196d83dbcc92d0955812ebcb767
olaalnajjar <69318042+olaalnajjar@users.noreply.github.com>
2020-09-29 14:12:01 +0200

    New Movie Data Class for top rated Database

---
+++ app/src/main/java/com/reem/android/finalmovieapp/data/models/ui/MovieT.kt
@@ -0,0 +1,24 @@
+package com.reem.android.finalmovieapp.data.models.ui
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+
+@Entity(tableName = "moviesT_table")
+data class MovieT (
+    @PrimaryKey
+    @SerializedName("id")
+    val id: Long,
+    @SerializedName("title")
+    val title: String,
+    @SerializedName("overview")
+    val overview: String,
+    @SerializedName("poster_path")
+    val posterPath: String,
+    @SerializedName("backdrop_path")
+    val backdropPath: String,
+    @SerializedName("vote_average")
+    val rating: Float,
+    @SerializedName("release_date")
+    val releaseDate: String
+)

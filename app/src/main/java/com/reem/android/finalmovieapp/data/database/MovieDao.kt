a4d0eb88933b6fb539160b381d520f6bd27064ed
olaalnajjar <69318042+olaalnajjar@users.noreply.github.com>
2020-09-29 13:36:53 +0200

    Making Dao Class

--- app/src/main/java/com/reem/android/finalmovieapp/data/database/MovieDao.kt
+++ app/src/main/java/com/reem/android/finalmovieapp/data/database/MovieDao.kt
@@ -1,4 +1,23 @@
 package com.reem.android.finalmovieapp.data.database
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.reem.android.finalmovieapp.data.models.ui.Movie
+
+@Dao
 interface MovieDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertMovie(movie: Movie)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(movies: List<Movie>)
+
+    @Query("SELECT * FROM movies_table")
+    fun getAllMovies(): List<Movie>
+
+
+
+
 }

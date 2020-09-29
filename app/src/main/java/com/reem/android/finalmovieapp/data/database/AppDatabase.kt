33daeb603a8ae630eca71bd7d8061eb61aa4958a
olaalnajjar <69318042+olaalnajjar@users.noreply.github.com>
2020-09-29 14:16:20 +0200

    Adding MovieT Entity for new Top Rated DataBase in AppDatabase.kt

--- app/src/main/java/com/reem/android/finalmovieapp/data/database/AppDatabase.kt
+++ app/src/main/java/com/reem/android/finalmovieapp/data/database/AppDatabase.kt
@@ -5,9 +5,10 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.reem.android.finalmovieapp.data.models.ui.Movie
+import com.reem.android.finalmovieapp.data.models.ui.MovieT
 
 
-@Database(entities = [Movie::class], version = 1, exportSchema = false)
+@Database(entities = [Movie::class, MovieT::class], version = 1, exportSchema = false)
 
 abstract class AppDatabase : RoomDatabase() {
 

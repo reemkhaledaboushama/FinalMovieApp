df94d95c73a28187a0b3936216be2f33afec0aab
olaalnajjar <69318042+olaalnajjar@users.noreply.github.com>
2020-09-29 14:17:56 +0200

    Changing response in TopRatedViewModel.kt to New GeMoviesResponseT and corresponding MovieT Classes

--- app/src/main/java/com/reem/android/finalmovieapp/ui/movie/TopRatedViewModel.kt
+++ app/src/main/java/com/reem/android/finalmovieapp/ui/movie/TopRatedViewModel.kt
@@ -4,11 +4,12 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import com.reem.android.finalmovieapp.data.models.ui.Movie
+import com.reem.android.finalmovieapp.data.models.ui.MovieT
 import com.reem.android.finalmovieapp.data.repository.MoviesRepository
 
 class TopRatedViewModel(application: Application) : AndroidViewModel(application) {
 
-    fun getTopRatedMovies(): LiveData<MutableList<Movie>> {
+    fun getTopRatedMovies(): LiveData<MutableList<MovieT>> {
         return MoviesRepository.getTopRatedMovies()
     }
 }

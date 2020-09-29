95130026e4f96aa4865e4e885f148d3bbf2bf89b
olaalnajjar <69318042+olaalnajjar@users.noreply.github.com>
2020-09-29 13:37:38 +0200

    Adding Entity and Primary key to Movie Data Class

--- app/src/main/java/com/reem/android/finalmovieapp/data/models/ui/Movie.kt
+++ app/src/main/java/com/reem/android/finalmovieapp/data/models/ui/Movie.kt
@@ -1,8 +1,14 @@
 package com.reem.android.finalmovieapp.data.models.ui
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+
+@Entity(tableName = "movies_table")
+
 data class Movie(
+    @PrimaryKey
     @SerializedName("id")
     val id: Long,
     @SerializedName("title")

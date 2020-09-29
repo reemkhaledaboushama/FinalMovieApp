7e226e51058648142c7185bf9dfee773fb86e4d2
olaalnajjar <69318042+olaalnajjar@users.noreply.github.com>
2020-09-29 13:36:15 +0200

    Making Database Class

--- app/src/main/java/com/reem/android/finalmovieapp/data/database/AppDatabase.kt
+++ app/src/main/java/com/reem/android/finalmovieapp/data/database/AppDatabase.kt
@@ -1,4 +1,34 @@
 package com.reem.android.finalmovieapp.data.database
 
-abstract class AppDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.reem.android.finalmovieapp.data.models.ui.Movie
+
+
+@Database(entities = [Movie::class], version = 1, exportSchema = false)
+
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun getMoviesDao(): MovieDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null)
+                return tempInstance
+
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext, AppDatabase::class.java, "movies_db"
+                ).allowMainThreadQueries().build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 }
